52d3221a46e2df5d69747bdc5e6af78ac0c3b359
“Daisuke” <dainoheya@gmail.com>
2019-01-13 17:56:14 +0900

    フォームの修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,15 +3,14 @@ class HomeController < ApplicationController
   def index
    # 入力画面を表示
    @inquiry = Inquiry.new
-   render :action => 'index'
  end
 
  def confirm
    # 入力値のチェック
-   @inquiry = Inquiry.new(params[:inquiry])
+   @inquiry = Inquiry.new(inquiry_params)
    if @inquiry.valid?
      # OK。確認画面を表示
-     render :action => 'confirm'
+     render :action => 'thanks'
    else
      # NG。入力画面を再表示
      render :action => 'index'
@@ -26,5 +25,11 @@ class HomeController < ApplicationController
    # 完了画面を表示
    render :action => 'thanks'
  end
+
+ private
+
+ def inquiry_params
+    params.require(:inquiry).permit(:name,:content,:email,:adress,:message,:phone,:kana)
+  end
 
 end
--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,10 +1,10 @@
-class Inquiry < ApplicationRecord
+class Inquiry
   include ActiveModel::Model
 
    attr_accessor :name,:kana,:email,:phone,:address,:message
 
-   validates :name, :presence => {:message => '名前を入力してください'}
-   validates :email, :presence => {:message => 'メールアドレスを入力してください'}
-   validates :phone, :presence => {:message => '電話番号を入力してください'}
+   validates :name,    :presence => {:message => '名前を入力してください'}
+   validates :email,   :presence => {:message => 'メールアドレスを入力してください'}
+   validates :phone,   :presence => {:message => '電話番号を入力してください'}
    validates :message, :presence => {:message => '本文を入力してください'}
 end
